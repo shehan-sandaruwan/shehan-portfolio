ba2a76899ec95b5ac6307ab7c3dd4b85510457a9
shehand-xino <shehand@xinotechsoftware.com>
2023-02-01 14:42:13 +0530

    added seo content

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,16 @@ function MyApp({ Component, pageProps }) {
     <React.Fragment>
       <Head>
         <title>{"shehan.is"}</title>
+        <meta name="google" content="nositelinkssearchbox" key="sitelinks" />
+        <meta name="google" content="notranslate" key="notranslate" />
+        <meta
+          name="description"
+          content="Shehan Dinuka is a Software Engineer Specilalized on front end development, He has obtained more than 4 years of experience developing web application from e-commerse to NFT based."
+        />
+        <meta
+          name="keywords"
+          content="Shehan Dinuka, Frontend developer, React Developer"
+        />
         <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
